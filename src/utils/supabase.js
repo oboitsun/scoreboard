fdcc6d3f488a5d3843f0d7980870f0072ace9adb
oboitsun <thewip3out@gmail.com>
2024-05-11 00:23:49 +0300

    removed some

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-console.log(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
