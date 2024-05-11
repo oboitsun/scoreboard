4ad9753ae03503a4bcee502a4b5a535ed321c990
oboitsun <thewip3out@gmail.com>
2024-05-11 13:48:33 +0300

    shortcircuit all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { MATCH_PAGE } from "./constants";
-import ControlLayout from "./layouts/ControlLayout";
-import FrameLayout from "./layouts/FrameLayout";
 import MainLayout from "./layouts/MainLayout";
-import Home from "./pages/Home";
-import Match from "./pages/Match";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         {/* <Route path={SIGNIN_PAGE} element={<SigninPage />} /> */}
-        <Route element={<MainLayout />}>
-          <Route path={"/current-match"} element={<Home />} />
+        <Route index element={<MainLayout />}>
+          {/* <Route path={"/current-match"} element={<Home />} /> */}
         </Route>
-        <Route index element={<FrameLayout />} />
+        {/* <Route index element={<FrameLayout />} /> */}
 
-        <Route element={<ControlLayout />}>
+        {/* <Route element={<ControlLayout />}>
           <Route path={`${MATCH_PAGE}/:matchId`} element={<Match />} />
-        </Route>
+        </Route> */}
       </Routes>
     </div>
   );
