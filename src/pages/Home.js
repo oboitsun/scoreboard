595cdd9199190552d374517bd221a70ba53d63dd
oboitsun <thewip3out@gmail.com>
2024-06-07 09:01:09 +0300

    check core js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { supabase } from "../utils/supabase";
 
 export default function Home() {
+  const [boards, setBoards] = useState([]);
   useEffect(() => {
     async function getTodos() {
-      let {
-        data: { user: user },
-        error,
-      } = await supabase.auth.getUser();
-      if (!error) {
-        let { data: account } = await supabase.from("Users").select().eq("id", user.id);
-        console.log(account);
-      }
+      let { data: scoreboards } = await supabase.from("scoreboard").select("*");
+      setBoards(scoreboards);
+      // let {
+      //   data: { user: user },
+      //   error,
+      // } = await supabase.auth.getUser();
+      // if (!error) {
+
+      //   console.log(account);
+      // }
     }
     getTodos();
   }, []);
   return (
     <div className="flex items-center justify-center flex-col gap-4 w-full max-w-24 mx-auto">
+      {boards.map((b) => (
+        <Link key={b.id} to={`screen/${b.id}`}>
+          Match {b.id}
+        </Link>
+      ))}
       {/* <Link
         className="w-full text-center p-2 uppercase rounded-lg bg-green-400 text-xl"
         to={SIGNIN_PAGE}>
